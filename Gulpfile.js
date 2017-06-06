43857e56b3ad4562151ecb5dfc98d10b4995c4b6
kaneshirokelli <kaneshirokelli@gmail.com>
2017-06-05 22:40:39 -0700

    npm install

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,11 @@
 "use strict";
-
 var gulp = require("gulp"),
 		browserSync = require("browser-sync"),
 		sass = require("gulp-sass"),
 		bourbon = require("node-bourbon").includePaths,
-		neat = require("node-neat").includePaths;
+		neat = require("node-neat").includePaths,
+		concat = require('gulp-concat');
+
 
 // Compiles all gulp tasks
 gulp.task("default", ["sass"]);
@@ -12,6 +13,7 @@ gulp.task("default", ["sass"]);
 // Live reload anytime a file changes
 gulp.task("watch", ["browserSync", "sass"], function() {
 	gulp.watch("src/scss/**/*.scss", ["sass"]);
+	gulp.watch("src/js/*.js", ['js']);
 	gulp.watch("dist/*.html").on("change", browserSync.reload);
 });
 
@@ -22,6 +24,12 @@ gulp.task("browserSync", function() {
 			baseDir: "dist"
 		}
 	})
+});
+
+gulp.task('js', function(){
+	gulp.src("src/js/*.js")
+		.pipe(concat('scripts.js'))
+		.pipe(gulp.dest('dist/js'))
 });
 
 // Compile SASS files
